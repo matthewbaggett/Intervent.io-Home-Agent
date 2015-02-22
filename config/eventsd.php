ea473468522e4f082586de54e8a120aa72ea882f
Matthew Baggett <matthew@baggett.me>
2015-02-22 05:00:54 +0000

    dev ip address

--- config/eventsd.php
+++ config/eventsd.php
@@ -1,7 +1,7 @@
 <?php
 
 \Eventsd\Eventsd::configure(array(
-  'Server' => "localhost",
+  'Server' => "10.0.0.96",
   'Port' => \Eventsd\Eventsd::DefaultPort,
   'Key' => 'fe451eb5-6c97-491e-9a66-23c7b4d528aa',
 ));
