1603866a28a77c693a64b3ef2d7c4817af018814
Matthew Baggett <matthew@baggett.me>
2015-02-22 03:58:01 +0000

    Warn about missing composer

--- agent.php
+++ agent.php
@@ -1,4 +1,9 @@
 <?php
+chdir(dirname(__FILE__));
+
+if(!file_exists('./vendor/autoload.php')){
+  die("You need to run <em>php composer.phar update</em> in the root directory.");
+}
 require_once("vendor/autoload.php");
 require_once("config/env.php");
 require_once("config/database.php");
