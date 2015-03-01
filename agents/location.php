b0745960e221ff0a52c5120d5deda7ff257be808
Matthew Baggett <matthew@baggett.me>
2015-03-01 03:18:10 +0000

    Via file this time.

--- agents/location.php
+++ agents/location.php
@@ -1,7 +1,13 @@
 <?php
 
 $loop->addPeriodicTimer(1, function() {
-  exec("gpspipe -w -n 10 | grep -m 1 lon", $output);
-  var_dump($output);exit;
+  exec("gpspipe -w -n 10 | grep -m 1 lon > /tmp/gpslocation.json");
+  usleep(5000);
+  $json = file_get_contents("/tmp/gpslocation.json");
+  unlink("/tmp/gpslocation.json");
+  $location = json_decode($json);
+  var_dump($location);
+  exit;
+
 });
 
